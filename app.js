4c59307d424b1c99ba3d1f383cae5bf9f367d780
neha3001 <ssneha3040@gmail.com>
2023-03-04 18:07:13 +0530

    sec

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client } = require("pg");
+/*const { Client } = require("pg");
 
 
 const client = new Client(
@@ -131,7 +131,7 @@ app.delete('/mobiles/:model', (req, res)=> {
    
 
 
-
+*/
 
 /*
 app.post("/svr/mobiles",function(req,res){
@@ -166,3 +166,234 @@ app.delete("/svr/mobiles/:id",function(req,res){
         else res.send(result)
     })
 })*/
+
+
+
+/*let mysql=require("mysql")
+let connData={
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"testDB"
+}
+let express=require("express")
+let app=express()
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+
+res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,PUT,PATCH,DELETE,HEAD"
+)
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,accept"
+    )
+    next();
+    
+})
+let cors = require('cors')
+app.use(cors())
+app.use(express.json())
+const port=2410
+app.listen(port,()=>console.log(`Node app are listening on port ${port}`))
+
+app.get("/mobiles",function(req,res){
+    let connection=mysql.createConnection(connData)
+    let sql="SELECT * FROM mobdata"
+    connection.query(sql,function(err,result){
+        if(err) res.status(404).send(err)
+        else res.send(result)
+    })
+})
+
+app.get("/mobiles/:name",function(req,res){
+    let name=req.params.name
+    console.log(name)
+    let connection=mysql.createConnection(connData)
+    let sql=`SELECT * FROM mobdata where name='${name}'`
+    connection.query(sql,function(err,result){
+        if(err) res.status(404).send("Not found")
+        else res.send(result)
+    })
+})
+
+
+app.post("/mobiles",function(req,res){
+
+    let body=req.body
+    console.log(body)
+    value=[body.name,body.price,body.brand,body.RAM,body.ROM,body.OS]
+    let connection=mysql.createConnection(connData)
+    let sql="INSERT INTO mobdata(name, price,brand,RAM,ROM,OS) VALUES(?,?,?,?,?,?)"
+connection.query(sql,value,function(err,result){
+    if(err) res.status(404).send(err)
+    else res.send(result)
+})
+
+})
+
+app.put("/mobiles/:name",function(req,res){
+    let name=req.params.name
+    let body=req.body
+    let connection=mysql.createConnection(connData)
+    let sql=`UPDATE mobdata SET name=?, brand=?,price=?,RAM=?,ROM=?,OS=? WHERE name='${name}'`
+    connection.query(sql,[ body.name,body?.brand,body?.price,body?.RAM,body?.ROM,body?.OS],function(err,result){
+if(err) res.status(404).send(err)
+else res.send(result)
+    })
+})
+app.delete("/mobiles/:name",function(req,res){
+    let name=req.params.name
+    console.log(name)
+    let connection=mysql.createConnection(connData)
+    let sql=`DELETE FROM mobdata WHERE name='${name}'`
+    connection.query(sql,function(err,result){
+        if(err)  res.status(404).send(err)
+        else res.send(result)
+    })
+})*/
+
+
+
+
+/*let mysql=require("mysql")
+let connData={
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"testDB"
+}*/
+const { Client } = require("pg");
+
+
+const client = new Client(
+    {
+    /*user: "postgres",
+    password: "root",
+    database: "test_db",
+    port: 5432,
+    host: "localhost",*/
+
+    DB_Host:  process.env.DB_Host|| "localhost",
+    DB_User: process.env.DB_User||"postgres",
+    DB_Password:process.env.DB_Password||"root",
+    DB_Name:process.env.DB_Name||"test_db",
+    DB_Port:process.env.DB_Port|| 5432,
+    postgresql:'//postgres:AZpscjVNAjrw4P4DFLWW@containers-us-west-48.railway.app:7110/railway'
+   //ssl: { rejectUnauthorized: false },
+});
+client.connect(function (res, error) {console.log('connected')})
+
+let express=require("express")
+let app=express()
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+
+res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,PUT,PATCH,DELETE,HEAD"
+)
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,accept"
+    )
+    next();
+    
+})
+
+app.use(express.json())
+//const port=2410
+var port=process.env.PORT || 2410;
+app.listen(port,()=>console.log(`Node app are listening on port ${port}`))
+
+app.get("/mobiles", function (req, res, next) 
+{
+    console.log("Inside /users get api");
+    
+const query = `SELECT * FROM mobile;`;
+client.query(query, function (err, result)
+
+ {if (err) {
+    console.log(err)
+     res.status(400).send(err);}
+     else{
+
+     
+ res.send(result.rows); 
+ //console.log(result.rows)  
+} 
+//client.end()
+});
+});
+
+
+app.get("/mobiles/:name", function (req, res, next) 
+{
+    let name=req.params.name
+    console.log("Inside /users get api");
+    
+const query = ` SELECT * FROM mobile WHERE  name='${name}';`;
+client.query(query, function (err, result)
+
+ {if (err) {
+    console.log(err)
+     res.status(400).send(err);}
+     else{
+
+     
+ res.send(result.rows); 
+ //console.log(result.rows) 
+ } 
+//client.end();
+});
+});
+
+
+
+
+app.post('/mobiles', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `INSERT INTO mobile(name,price,brand,ram,rom,os) VALUES('${user.name}', '${user.price}', '${user.brand}','${user.ram}','${user.rom}','${user.os}');`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            console.log(result)
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    
+})
+
+app.put('/mobiles/:name', (req, res)=> {
+    let name=req.params.name
+    let user = req.body;
+    console.log(name)
+    //console.log(user)
+    let updateQuery = `UPDATE mobile SET name='${user.name}',price=${user.price},brand='${user.brand}',ram='${user.ram}',rom='${user.rom}',os='${user.os}' WHERE name='${name}'`;
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    
+})
+
+
+
+app.delete('/mobiles/:name', (req, res)=> {
+    let insertQuery = `delete from mobile where name='${req.params.name}'`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+})
+
